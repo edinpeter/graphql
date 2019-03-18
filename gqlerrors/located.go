dcad68a029d8c55c3d84cb1075b0cdd2107c0cd8
edinpeter <edin.3@osu.edu>
2019-03-17 20:56:40 -0400

    Changed imports on gqlerros

--- gqlerrors/located.go
+++ gqlerrors/located.go
@@ -2,7 +2,7 @@ package gqlerrors
 
 import (
 	"errors"
-	"github.com/graphql-go/graphql/language/ast"
+	"github.com/edinpeter/graphql/language/ast"
 )
 
 // NewLocatedError creates a graphql.Error with location info
